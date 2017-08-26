c052205a6baec5704be1d449d50aa480c143b222
Ella Rose <python_pride@protonmail.com>
2017-08-26 15:30:47 -0700

    ttl/packet_id added

--- simulator.py
+++ simulator.py
@@ -43,22 +43,23 @@ class Broadcast_Network_Simulator(pride.components.base.Base):
         
     @classmethod
     def unit_test(cls):           
-        node_types = [Broadcast_Node_Simulator]
+        node_types = [Protocol_Simulator]
         group_types = [Node_Group]
         network_size = 2
         simulator = cls.generate_random_network(node_types, group_types, network_size)        
-        connecting_node = Broadcast_Node_Simulator(latency=.005, name="router")
+        connecting_node = Protocol_Simulator(latency=.005, name="router")        
         simulator.groups[0].add(connecting_node)
         simulator.groups[1].add(connecting_node)
-      #  simulator.groups[2].add(connecting_node)
+        #simulator.groups[2].add(connecting_node)
         print "Name of routing node: {}".format(connecting_node)
-        for count in range(16):            
+        while True:
+            #connecting_node.outgoing_packets.append("Router Beacon")
             simulator.run()
         
     
 class Packet(object):
         
-    header = ("sender_id", "timestamp", "message_counter", "data")
+    header = ("sender_id", "timestamp", "message_counter", "time_to_live", "data")
     
     def __init__(self, **kwargs):
         super(Packet, self).__init__()
@@ -67,22 +68,29 @@ class Packet(object):
                 value = kwargs.pop(field_name)
             except KeyError:
                 raise ValueError("Attempted to create invalid packet; Missing field '{}'".format(field_name))
-            setattr(self, field_name, value)
-            
+            setattr(self, field_name, value)            
+        
     def __str__(self):        
         field_values = ', '.join("{}={}".format(field_name, getattr(self, field_name)) for field_name in self.header)
         return self.__class__.__name__ + '(' + field_values + ')'
+        
+    def copy(self):
+        attributes = dict((field_name, getattr(self, field_name)) for field_name in self.header)
+        return type(self)(**attributes)
         
         
 class Broadcast_Node_Simulator(pride.components.base.Base):
     
     packet_type = Packet
+    packet_fields = {"sender_id" : "name", "timestamp" : "simulation_time",
+                     "message_counter" : "_counter", "time_to_live" : "_counter_size"}
+    
     latency_categories = {"fast" : .025, "normal" : .1, "slow" : .75}
     defaults = {"simulation_time" : 0.00, "latency" : 0.100, "_counter" : 0}
-    flags = {"name" : None, "_next_send_time" : 0.00}
-    mutable_defaults = {"groups" : list}
+    flags = {"name" : None, "_next_send_time" : 0.00, "_counter_size" : 255}
+    mutable_defaults = {"groups" : list, "outgoing_packets" : list}
     verbosity = {"update_state" : "vvv"}
-    
+        
     def __init__(self, *args, **kwargs):
         super(Broadcast_Node_Simulator, self).__init__(*args, **kwargs)
         self.name = self.name or id(self)       
@@ -92,19 +100,39 @@ class Broadcast_Node_Simulator(pride.components.base.Base):
         #print("{} waiting for {}; current: {}; ready: {}".format(self.name, self._next_send_time, self.simulation_time, self._next_send_time <= self.simulation_time))        
         if self._next_send_time <= self.simulation_time:            
             self._next_send_time = self.simulation_time + self.latency
-            packet = self.broadcast('')            
-            for node in get_nodes_from_groups(self.groups):
-                if node is not self:
-                    self.alert("Sending to: {}".format(node), display_name=self, level=self.verbosity["update_state"])
-                    node.receive_packet(packet)
-    
-    def broadcast(self, data):
-        self._counter += 1
-        return self.packet_type(sender_id=self.name, timestamp=self.simulation_time, 
-                                message_counter=self._counter, data=data)        
-           
-    def receive_packet(self, packet):
-        print("{} received: {}".format(self.name, packet))
+            for packet in self.outgoing_packets:
+                assert isinstance(packet, Packet)
+                self._broadcast(packet)
+            del self.outgoing_packets[:]
+            
+    def create_packet(self, data):
+        assert not isinstance(data, Packet)
+        self._counter = (self._counter + 1) % self._counter_size
+        packet_kwargs = dict((key, getattr(self, attribute_name)) for key, attribute_name in self.packet_fields.items())        
+        packet_kwargs["data"] = data        
+        return self.packet_type(**packet_kwargs)  
+        
+    def broadcast(self, packet):
+        assert isinstance(packet, Packet)
+        self.outgoing_packets.append(packet)                
+        
+    def _broadcast(self, packet):      
+        assert isinstance(packet, Packet)
+        for node in get_nodes_from_groups(self.groups):
+            if node is not self:
+                self.alert("Sending to: {}".format(node), display_name=self, level=0)#self.verbosity["update_state"])                        
+                node.receive_packet(packet.copy())                  
+        
+    def receive_packet(self, packet):                
+        packet.time_to_live -= 1
+        assert packet.time_to_live >= 0
+        #if packet.recipient == self.name:
+        self.handle_received_packet(packet)            
+        if packet.time_to_live > 0:
+            self.broadcast(packet)
+            
+    def handle_received_packet(self, packet):
+        self.alert("received: {}".format(packet))    
         
     @classmethod
     def generate_random_node(cls, **kwargs):
@@ -165,19 +193,46 @@ class Node_Group(pride.components.base.Base):
 
 class Protocol_Packet(Packet):
     
-    header = ("sender_id", "timestamp", "counter", "routing_method", "request_type")
+    header = ("time_to_live", "data", "packet_id")
     
     
 class Protocol_Simulator(Broadcast_Node_Simulator):
       
     ROUTING_METHOD = {"specific recipient" : 0x00, "broadcast" : 0x01,
                       "multicast" : 0x01}
-                      
-    def broadcast(self, data):
-        self._counter += 1
-        packet_info = {"sender_id" : self.name, "timestamp" : self.simulation_time,
-                       "counter" : self._counter, "routing_method" : 0x01}
+    packet_type = Protocol_Packet
+    packet_fields = {"time_to_live" : "time_to_live"}
     
+    defaults = {"time_to_live" : 255}       
+    mutable_defaults = {"recently_sent_packets" : list}
+    
+    def __init__(self, *args, **kwargs):
+        super(Protocol_Simulator, self).__init__(*args, **kwargs)
+        packet_data = "Announce: {}".format(self.reference)
+        packet_id = self.create_packet_id(packet_data)
+        self.outgoing_packets.append(Protocol_Packet(time_to_live=self.time_to_live, 
+                                                     data=packet_data,
+                                                     packet_id=packet_id))
+        
+    def broadcast(self, packet):
+        #print "Broadcasting: ", packet.packet_id, self.recently_sent_packets
+        #raw_input('')
+        if packet.packet_id not in self.recently_sent_packets:
+            self.outgoing_packets.append(packet)
+            self.recently_sent_packets.append(packet.packet_id)
+      #      self.sent_packets_ttl[packet.packet_id] = packet.time_to_live
+      #  else:
+      #      self.
+    def create_packet(self, data):
+        assert not isinstance(data, Packet)        
+        packet_kwargs = dict((key, getattr(self, attribute_name)) for key, attribute_name in self.packet_fields.items())        
+        packet_kwargs["data"] = data        
+        packet_kwargs["packet_id"] = self.create_packet_id(data)
+        return self.packet_type(**packet_kwargs)  
+        
+    def create_packet_id(self, data):
+        return hash(data) 
+        
 if __name__ == "__main__":
     Broadcast_Network_Simulator.unit_test()
     
